30a4ae7b5a8431e451840ef595566b91eb922d89
Unknown <gdionisi@lbl.gov>
2017-10-25 18:22:09 +0200

    Add Google Analytics

--- wp-content/themes/Landy-child/header.php
+++ wp-content/themes/Landy-child/header.php
@@ -1,6 +1,17 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108620700-1"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-108620700-1');
+	</script>
+
+
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />	
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
