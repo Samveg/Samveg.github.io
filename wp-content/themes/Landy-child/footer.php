f6c6a79f7754688697ee3f133d99e640576e4686
Unknown <gdionisi@lbl.gov>
2017-10-27 12:38:18 +0200

    Fix typo

--- wp-content/themes/Landy-child/footer.php
+++ wp-content/themes/Landy-child/footer.php
@@ -212,7 +212,7 @@
 	<script>
 		jQuery(document).ready( function() {
 		    jQuery( ".menu-main-menu-container li a" ).click( function() {
-		        var id = this.getAttribute('href', 2);;
+		        var id = this.getAttribute('href', 2);
 		        var offset = -60;
 		        jQuery( 'html, body' ).animate({
 		            scrollTop: jQuery( id ).offset().top + offset
